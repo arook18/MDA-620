8a278ec7c45102dd546e8fe2640a2cda16faac07
arook18 <113156248+arook18@users.noreply.github.com>
2022-09-15 20:34:18 -0400

    Add files via upload

---
+++ Pandas Codes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+pd.Series([1, 3, 5, np.nan, 6, 8])
+
+
+# In[5]:
+
+
+dates = pd.date_range("20130101", periods=6)
+
+
+# In[6]:
+
+
+dates
+
+
+# In[ ]:
+
+
+
+
